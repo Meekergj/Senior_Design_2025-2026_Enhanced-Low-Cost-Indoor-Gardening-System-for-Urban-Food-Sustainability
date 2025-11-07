12c566a5796e3bc39d306175fe7a517cf4cdaed8
Gmann620 <gmeeker622@gmail.com>
2025-11-06 21:29:10 -0500

    Created function neural network

---
+++ Indoor-Gardening-System/build_model.py
@@ -0,0 +1,71 @@
+# Copyright info and other stuff
+#
+#
+
+import tensorflow as tf
+
+from keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
+# Get images and labels as training and validation data
+def load(data_path, image_height, image_width):
+    # From the folder data_path get images from subfolders (labels = subfolder name)
+    train_set = tf.keras.utils.image_dataset_from_directory(
+                data_path,
+                validation_split=0.3,
+                subset="training",
+                seed=123,
+                image_size=(image_height, image_width))
+    
+    validation_set = tf.keras.utils.image_dataset_from_directory(
+                data_path,
+                validation_split=0.3,
+                subset="validation",
+                seed=123,
+                image_size=(image_height, image_width))
+    
+    # Tensorflow magic to cache and prefetch this data, increase performance
+    AUTOTUNE = tf.data.AUTOTUNE
+    train_set = train_set.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+    validation_set = validation_set.cache().prefetch(buffer_size=AUTOTUNE)
+
+    # convert pixels from [0 255] to [0 1], easier on neural net
+    normalization_layer = tf.keras.layers.Rescaling(1./255)
+    normalized_set = train_set.map(lambda x, y: (normalization_layer(x), y))
+
+    return normalized_set, validation_set
+
+# Convolutional Neural Net
+def build(train_set, validation_set, input_shape, num_classes):
+    model = models.Sequential()
+
+    # Slightly randomizes the input to prevent overfitting and increase accuracy
+    model.add(layers.Input(input_shape))
+    model.add(layers.RandomFlip("horizontal"))
+    model.add(layers.RandomRotation(0.2))
+    model.add(layers.RandomZoom(0.2))
+
+    # Layers of the CNN model
+    model.add(layers.Conv2D(16, 3, activation='relu', input_shape=input_shape))
+    model.add(layers.MaxPooling2D(2))
+    model.add(layers.Conv2D(16, 3, activation='relu', input_shape=input_shape))
+    model.add(layers.MaxPooling2D(2))
+    model.add(layers.Conv2D(32, 3, activation='relu'))
+    model.add(layers.MaxPooling2D(2))
+    model.add(layers.Conv2D(64, 3, activation='relu'))
+    model.add(layers.MaxPooling2D(2))
+    model.add(layers.Conv2D(128, 3, activation='relu'))
+    model.add(layers.Dropout(0.2))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(32, activation='relu'))
+    model.add(layers.Dense(num_classes))
+
+    # Take the aformentioned layers of the model and compile together
+    model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+    model.summary()
+
+    return model
--- Indoor-Gardening-System/main.py
+++ Indoor-Gardening-System/main.py
@@ -5,17 +5,63 @@
 import tensorflow as tf
 
 from keras import datasets, layers, models
+import build_model as bm
 import matplotlib.pyplot as plt
+from pathlib import Path
 
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+#-----------------------------------------------------------#
+# Variables / Parameters
+#-----------------------------------------------------------#
+image_height = 128
+image_width = 128
+shape = (image_height, image_width, 3)
+labels = ["healthy", "diseased"]
+labels_size = len(labels)
 
-model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dense(10))
+CURRENT_DIR = Path.cwd()
+DATA_DIR = CURRENT_DIR / "data" / "Gauva (P3)"
+CHECKPOINT_DIR = CURRENT_DIR / "models" / "checkpoints"
+EXPORT_DIR = CURRENT_DIR / "tests" / "models"
 
-model.summary()
+#-----------------------------------------------------------#
+# Build and Compile Model
+#-----------------------------------------------------------#
+#data_path = tf.keras.utils.get_file(DATA_DIR, extract=True)
+#data_path = Path(data_path).with_suffix('')
+train_set, validation_set = bm.load(DATA_DIR, image_height, image_width)
+model = bm.build(train_set, validation_set, shape, labels_size)
+
+#-----------------------------------------------------------#
+# Train Model
+#-----------------------------------------------------------#
+epochs = 10
+history = model.fit(
+  train_set,
+  validation_data=validation_set,
+  epochs=epochs
+)
+
+#-----------------------------------------------------------#
+# Visualize (from https://www.tensorflow.org/tutorials/images/classification)
+#-----------------------------------------------------------#
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
